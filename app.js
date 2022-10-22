855e1f1f342ffd5793a0419c1f0d5737127e5f5c
平野真一 <hiranoshinichi@hiranoshinichi-no-iMac.local>
2022-10-22 12:34:13 +0900

    テスト完了です

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ $("#addTodo").click(function(){
 });
 
 
+
+
+$(document).on("change","input[type= checkbox]",function(){
+    if($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+    }else {
+        $(this).parent().css("text-decoration", "none");
+
+    }
+});
